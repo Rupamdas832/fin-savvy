0a19df2e03d08deedac06118a6fde5b399a90162
Rupam Das <rupamdas832@gmail.com>
2023-08-18 12:48:00 +0530

    Headers source fixed

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/about",
+        source: "/(.*)",
         headers: [
           {
             key: "X-Frame-Options",
